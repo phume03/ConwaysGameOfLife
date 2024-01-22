6e11b7f798697e3c479461b98a524aa918efbb33
Phumelela Mdluli <pumie03@gmail.com>
2024-01-22 11:10:36 +0200

    updated mit license

--- testing_JQP_final.py
+++
@@ -1,6 +0,0 @@
-JOHN_DATA = {
-    'name': 'John Q. Public',
-    'address': '123 Main St. \nAnytown, FL 99999'
-    'siblings': ['Michael R. Public', 'Suzy Q. Public'],
-    'parents': ['John Q. Public Sr.', 'Mary S. Public'],
-}
--- testing_JQP_initial.py
+++
@@ -1,9 +0,0 @@
-JOHN_DATA = {
-    'name': 'John Q. Public',
-    'street': '123 Main St.',
-    'city': 'Anytown',
-    'state': 'FL',
-    'zip': 99999,
-    'siblings': ['Michael R. Public', 'Suzy Q. Public'],
-    'parents': ['John Q. Public Sr.', 'Mary S. Public'],
-}
